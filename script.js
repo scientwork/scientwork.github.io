0d317c12ff26ae1a4dc6ca4accee770dd1f04d5f
scientwork <93660803+scientwork@users.noreply.github.com>
2021-11-03 16:48:12 +0300

    add

---
+++ script.js
@@ -0,0 +1,125 @@
+jQuery('document').ready(function() {
+	jQuery('#q1').on('click', function() {
+		jQuery('.description').html(jQuery(this).attr('description-data'));
+		jQuery('.description').fadeIn();
+		for(let i = 1; i <= 15; i++) {
+			jQuery('#q' + i.toString()).css('opacity','0');
+		}
+		jQuery('#q1').css('opacity','0.5');
+	});
+	jQuery('.exit').on('click', function() {
+		jQuery('.description').fadeOut();
+	});
+	jQuery('#q2').on('click', function() {
+		jQuery('.description').html(jQuery(this).attr('description-data'));
+		jQuery('.description').fadeIn();
+		for(let i = 1; i <= 15; i++) {
+			jQuery('#q' + i).css('opacity','0');
+		}
+		jQuery('#q2').css('opacity','0.5');
+	});
+	jQuery('.exit').on('click', function() {
+		jQuery('.description').fadeOut();
+	});
+	jQuery('#q3').on('click', function() {
+		jQuery('.description').html(jQuery(this).attr('description-data'));
+		jQuery('.description').fadeIn();
+		for(let i = 1; i <= 15; i++) {
+			jQuery('#q' + i).css('opacity','0');
+		}
+		jQuery('#q3').css('opacity','0.5');
+	});
+	jQuery('.exit').on('click', function() {
+		jQuery('.description').fadeOut();
+	});
+	jQuery('#q4').on('click', function() {
+		jQuery('.description').html(jQuery(this).attr('description-data'));
+		jQuery('.description').fadeIn();
+		for(let i = 1; i <= 15; i++) {
+			jQuery('#q' + i).css('opacity','0');
+		}
+		jQuery('#q4').css('opacity','0.5');
+	});
+	jQuery('.exit').on('click', function() {
+		jQuery('.description').fadeOut();
+	});
+	jQuery('#q5').on('click', function() {
+		jQuery('.description').html(jQuery(this).attr('description-data'));
+		jQuery('.description').fadeIn();
+		for(let i = 1; i <= 15; i++) {
+			jQuery('#q' + i).css('opacity','0');
+		}
+		jQuery('#q5').css('opacity','0.5');
+	});
+	jQuery('.exit').on('click', function() {
+		jQuery('.description').fadeOut();
+	});
+	jQuery('#q6').on('click', function() {
+		jQuery('.description').html(jQuery(this).attr('description-data'));
+		jQuery('.description').fadeIn();
+		for(let i = 1; i <= 15; i++) {
+			jQuery('#q' + i).css('opacity','0');
+		}
+		jQuery('#q6').css('opacity','0.5');
+	});
+	jQuery('.exit').on('click', function() {
+		jQuery('.description').fadeOut();
+	});
+	jQuery('#q7').on('click', function() {
+		jQuery('.description').html(jQuery(this).attr('description-data'));
+		jQuery('.description').fadeIn();
+		for(let i = 1; i <= 15; i++) {
+			jQuery('#q' + i).css('opacity','0');
+		}
+		jQuery('#q7').css('opacity','0.5');
+	});
+	jQuery('.exit').on('click', function() {
+		jQuery('.description').fadeOut();
+	});
+	jQuery('#q8').on('click', function() {
+		jQuery('.description').html(jQuery(this).attr('description-data'));
+		jQuery('.description').fadeIn();
+		for(let i = 1; i <= 15; i++) {
+			jQuery('#q' + i).css('opacity','0');
+		}
+		jQuery('#q8').css('opacity','0.5');
+	});
+	jQuery('.exit').on('click', function() {
+		jQuery('.description').fadeOut();
+	});
+	jQuery('#q9').on('click', function() {
+		jQuery('.description').html(jQuery(this).attr('description-data'));
+		jQuery('.description').fadeIn();
+		for(let i = 1; i <= 15; i++) {
+			jQuery('#q' + i).css('opacity','0');
+		}
+		jQuery('#q9').css('opacity','0.5');
+	});
+	jQuery('.exit').on('click', function() {
+		jQuery('.description').fadeOut();
+	});
+	jQuery('#q10').on('click', function() {
+		jQuery('.description').html(jQuery(this).attr('description-data'));
+		jQuery('.description').fadeIn();
+		for(let i = 1; i <= 15; i++) {
+			jQuery('#q' + i).css('opacity','0');
+		}
+		jQuery('#q10').css('opacity','0.5');
+	});
+	jQuery('.exit').on('click', function() {
+		jQuery('.description').fadeOut();
+	});
+	jQuery('#q11').on('click', function() {
+		jQuery('.description').html(jQuery(this).attr('description-data'));
+		jQuery('.description').fadeIn();
+		for(let i = 1; i <= 15; i++) {
+			jQuery('#q' + i).css('opacity','0');
+		}
+		jQuery('#q11').css('opacity','0.5');
+	});
+	jQuery('.exit').on('click', function() {
+		jQuery('.description').fadeOut();
+	});
+	
+
+});
